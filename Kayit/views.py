5ae930961bd51d46ad8627bb6726786e03a6963a
VektorelBilisim <VektorelBilisim@vektorel.com>
2020-01-11 09:00:12 +0200

    11012020_2

--- Kayit/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import KayitModel
-
-def kayitListele(request):
-    kayitlar = KayitModel.objects.all()
-    return render(request,"index.html",{"kayitlar":kayitlar})
